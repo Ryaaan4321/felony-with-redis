fec6a9745d4e563c34a041ea290bec6d6adff751
ryaaan4321 <aryan056400@gmail.com>
2025-09-14 21:27:31 +0530

    shit-again

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,9 @@
 import * as net from "net";
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
+    connection.write(`+PONG\r\n`)
 });
 
 server.listen(6379, "127.0.0.1");
